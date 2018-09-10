947383003708a36e5bbca86c523705e33b2d8e1c
Costas-Zeimpekis <morpheustudio@gmail.com>
2018-09-10 10:43:41 +0300

    Solved reversestring

--- reversestring/index.js
+++ reversestring/index.js
@@ -6,10 +6,36 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+// Solution 1
+// function reverse(str) {
+//   const arr = str.split('');
+//   arr.reverse();
+
+//   return arr.join('');
+// }
+
+// Solution 2
+// function reverse(str) {
+//   return str
+//     .split('')
+//     .reverse()
+//     .join('');
+// }
+
+// Solution 3
+// function reverse(str) {
+//   let reversed = '';
+
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+
+//   return reversed;
+// }
+
+// Solution 4
 function reverse(str) {
-  let arr = str.split('');
-  arr.reverse();
-  str = arr.join('');
+  return str.split('').reduce((rev, char) => char + rev, '');
 }
 
 module.exports = reverse;
