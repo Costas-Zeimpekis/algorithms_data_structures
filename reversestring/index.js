cb2a1261521cf86333a8e7c4888f86e27492399a
Costas-Zeimpekis <morpheustudio@gmail.com>
2018-09-10 10:09:25 +0300

    First Commit

---
+++ reversestring/index.js
@@ -0,0 +1,15 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+  let arr = str.split('');
+  arr.reverse();
+  str = arr.join('');
+}
+
+module.exports = reverse;
