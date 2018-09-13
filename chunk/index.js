f82457cc1064527ec9fa85c6c30faa3ba5b5af3e
Costas-Zeimpekis <morpheustudio@gmail.com>
2018-09-13 15:30:15 +0300

    Solution chunk

--- chunk/index.js
+++ chunk/index.js
@@ -8,6 +8,53 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// // Solution 1
+// function chunk(array, size) {
+//   let arrParent = [];
+//   let arrChild = [];
+//   let iterator = 0;
+
+//   while (iterator < array.length) {
+//     let i = 0;
+//     for (i; i < size && iterator < array.length; i++) {
+//       arrChild.push(array[iterator]);
+//       iterator++;
+//     }
+//     arrParent.push(arrChild);
+//     arrChild = [];
+//   }
+
+//   return arrParent;
+// }
+
+//Solution 2
+// function chunk(array, size) {
+//   let chunked = [];
+
+//   for (let element of array) {
+//     const last = chunked[chunked.length - 1];
+//     if (!last || last.length === size) {
+//       chunked.push([element]);
+//     } else {
+//       last.push(element);
+//     }
+//   }
+
+//   return chunked;
+// }
+
+// Solution 3
+
+function chunk(array, size) {
+  const chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+
+  return chunked;
+}
 
 module.exports = chunk;
